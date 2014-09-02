7073aadf7d7099e7593b5ebee0ffc065efc6b1e0
Ganesh4 <ughadeganesh@gmail.com>
2014-09-02 16:43:53 +0530

    Update EmployeeCtrl.java

--- ROOT/WEB-INF/src/com/ls/controller/EmployeeCtrl.java
+++ ROOT/WEB-INF/src/com/ls/controller/EmployeeCtrl.java
@@ -1,26 +1,24 @@
 package com.ls.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.ls.dao.EmployeeDao;
 import com.ls.pojo.Location;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeCtrl {
-
+	
 	@Autowired
 	EmployeeDao employeeDao;
+	
 	@RequestMapping("/all")
 	 public ModelAndView getAll()
 	 {
 		 List<Location> list=null;
-		 
 		 list=employeeDao.getAllEmployee();
 		 System.out.println(list);
 		 return new ModelAndView("employee","listEmployee",list);
