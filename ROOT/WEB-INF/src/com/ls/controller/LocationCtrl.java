71a6afa91157764408e8d0689482d7a93619e088
Ganesh4 <ughadeganesh@gmail.com>
2014-09-02 16:57:17 +0530

    Update LocationCtrl.java

--- ROOT/WEB-INF/src/com/ls/controller/LocationCtrl.java
+++ ROOT/WEB-INF/src/com/ls/controller/LocationCtrl.java
@@ -1,7 +1,6 @@
 package com.ls.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -72,13 +71,9 @@ public class LocationCtrl {
 	//DELETE
 	@RequestMapping("/delete")
 	public ModelAndView deleteLoc(@RequestParam("id")Long id){
-		
 		locationDao.deleteDataById(id);	
 		List<Location> list=null;
 		list=locationDao.getAll();
 		return new ModelAndView("listall","list",list);
-
-	
-		
 	}
 }
